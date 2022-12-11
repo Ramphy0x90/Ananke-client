b688bd6d3accdfc087a17fe07edc73ceb65cdb5b
ramphy0x90 <ramphy11@hotmail.com>
2022-12-11 20:39:38 +0100

    Routing animations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './containers/home/home.component';
+import { LoginComponent } from './containers/login/login.component';
+import { TicketComponent } from './containers/ticket/ticket.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    TicketComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule
   ],
   providers: [],
