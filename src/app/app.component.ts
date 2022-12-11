6e46b893d63689ee440e415f5b9f08d777ff3080
ramphy0x90 <ramphy11@hotmail.com>
2022-12-11 21:30:06 +0100

    Styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,7 @@ import { RouteTransitionAnimations } from './route-transition-animations';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  styleUrls: ['./app.component.css', '../assets/style/_dark.css'],
   animations: [RouteTransitionAnimations]
 })
 export class AppComponent {
