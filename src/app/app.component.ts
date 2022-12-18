ae5d3dea5cf94638458a42ed0cbd008392829692
ramphy0x90 <ramphy11@hotmail.com>
2022-12-18 16:11:23 +0100

    General style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { RouteTransitionAnimations } from './route-transition-animations';
+import { DOCUMENT } from '@angular/common';
+
+let currentStyle: string = 'light';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css', '../assets/style/_dark.css'],
+  styleUrls: ['./app.component.css'],
   animations: [RouteTransitionAnimations]
 })
 export class AppComponent {
   title = 'Ananke-client';
+
+  constructor(@Inject(DOCUMENT) private document: Document) {
+    const head = this.document.getElementsByTagName('head')[0];
+    const style = this.document.createElement('link');
+
+    style.rel = 'stylesheet';
+    style.href = `../assets/style/_${currentStyle}.css`;
+
+    head.appendChild(style);
+  }
 
   prepareRoute(outlet: RouterOutlet) {
     return outlet && 
