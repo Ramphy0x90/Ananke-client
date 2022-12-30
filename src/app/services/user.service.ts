c10367616c4cafbd663d726ccf29f6ff9e1dccfd
Ramphy Aquino Nova <30904905+Ramphy0x90@users.noreply.github.com>
2022-12-30 15:16:55 +0100

    User service

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,9 +1,80 @@
-import { Injectable } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { User } from '../models/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
+  userStatus: EventEmitter<boolean> = new EventEmitter;
 
-  constructor() { }
+  private httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
+
+  constructor(private httpClient: HttpClient) { }
+
+  /**
+   * This function is used to autenticate an user
+   * @param user crednetials
+   * @returns 
+   */
+  logIn(user: User): Observable<any> {
+    return this.httpClient.post(
+      `${environment.server}/user/log-in`, user, this.httpOptions
+    );
+  }
+
+  /**
+   * This function is used to save on session
+   * the current logged user
+   * @param user data
+   * @param token JWT token
+   */
+  saveLogInUser(user: User, token: string): void {
+    window.sessionStorage.clear();
+
+    this.userStatus.emit(true);
+    window.sessionStorage.setItem('user', JSON.stringify(user));
+    window.sessionStorage.setItem('token', token);
+  }
+
+  /**
+   * This function returns
+   * the current logged user
+   * @returns user data
+   */
+  getLoggedUser(): User | null {
+    let user = window.sessionStorage.getItem('user');
+    if (user) return JSON.parse(user);
+
+    return null;
+  }
+
+  /**
+   * Get logged user token
+   * @returns JWT token
+   */
+  getLoggedUserToken(): string {
+    let token = window.sessionStorage.getItem('token');
+    return token ? token : '';
+  }
+
+  /**
+   * Check if a user is logged
+   * @returns boolean
+   */
+  isLoggedIn(): boolean {
+    return window.sessionStorage.getItem('user') !== null;
+  }
+
+  /**
+   * Log out user
+   */
+  logOut(): void {
+    this.userStatus.emit(false);
+    window.sessionStorage.clear();
+  }
 }
