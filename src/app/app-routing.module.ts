1490be56784be5c6866a0bf63047cebfe75585a1
ramphy0x90 <ramphy11@hotmail.com>
2023-02-19 15:10:54 +0100

    File paths refactoring

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,10 +4,10 @@ import { AnankeAppComponent } from './containers/app/app.component';
 import { HomeComponent } from './containers/home/home.component';
 import { LoginComponent } from './containers/login/login.component';
 import { DashboardComponent } from './containers/dashboard/dashboard.component';
-import { TicketsComponent } from './containers/tickets/tickets.component';
+import { TicketsComponent } from './containers/ticket/tickets/tickets.component';
 import { AuthGuard } from './guards/auth.guard';
-import { CreateTicketComponent } from './containers/create-ticket/create-ticket.component';
-import { RecentTicketsComponent } from './containers/recent-tickets/recent-tickets.component';
+import { CreateTicketComponent } from './containers/ticket/create-ticket/create-ticket.component';
+import { RecentTicketsComponent } from './containers/ticket/recent-tickets/recent-tickets.component';
 import { ClosedTicketsComponent } from './containers/ticket/closed-tickets/closed-tickets.component';
 import { TicketComponent } from './containers/ticket/ticket.component';
 
